c8f8c8b34d46ac4cd12e626822e38830ad286532
Lazy_V <954294627@qq.com>
2022-08-29 22:56:30 +0800

    Fixed Template

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8e$-nhtgh78t2^6j_g6-cka-#jcv6&k4*8*l76-a827@p^$zn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
